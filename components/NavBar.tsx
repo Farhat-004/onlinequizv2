d429ce9a88c470cbd8f7917f77905029a13b6e46
Farhatul Hassan <farhatulhassan1@gmail.com>
2026-04-11 17:27:57 +0600

    fix:  added ceate-quiz and exam option in navbar component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,6 +5,8 @@ export default function NavBar() {
     <nav className="w-full bg-white shadow p-4 flex justify-between items-center">
       <Link href={'/'} className="font-semibold">eExam</Link>
       <div className="flex gap-3">
+        <Link href={'/create-quiz'} className="text-sm text-gray-600">Create Quiz</Link>
+        <Link href={'/quiz'} className="text-sm text-gray-600">Exam</Link>
         <button className="text-sm text-gray-600">Feedback</button>
         <button className="text-sm text-gray-600">Report</button>
       </div>
